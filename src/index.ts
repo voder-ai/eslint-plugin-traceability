40761069fde4f17ed17110811b6a8ebcb057a8f1
Tom Howard <tom.howard@greater.com.au>
2025-11-16 12:29:44 +1100

    feat: register require-req-annotation rule in plugin exports

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@
  */
 
 import requireStoryAnnotation from "./rules/require-story-annotation";
+import requireReqAnnotation from "./rules/require-req-annotation";
 
 export const rules = {
   "require-story-annotation": requireStoryAnnotation,
+  "require-req-annotation": requireReqAnnotation,
 };
 
 export const configs = {
